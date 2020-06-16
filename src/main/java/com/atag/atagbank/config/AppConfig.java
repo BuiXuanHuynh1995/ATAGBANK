58607bf8fc5d76e0c780e2801cb46ae69fb1540c
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-06-16 23:29:35 +0700

    init project

---
+++ src/main/java/com/atag/atagbank/config/AppConfig.java
@@ -0,0 +1,22 @@
+package com.atag.atagbank.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.atag.atagbank.controller")
+
+public class AppConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+    private ApplicationContext appContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        appContext = applicationContext;
+    }
+}
