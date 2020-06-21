8617ca4bcefc049f0fa9fadc2bed7bd09e34c6f8
linhhm2406 <linh.hm24@gmail.com>
2020-06-21 23:42:23 +0700

    upload personal UI (make deposit)

--- src/main/java/com/atag/atagbank/controller/MainController.java
+++ src/main/java/com/atag/atagbank/controller/MainController.java
@@ -12,6 +12,6 @@ public class MainController {
 
     @GetMapping("/personal-profile")
     public String getPersonalProfile(){
-        return "personal/confirmOTP";
+        return "personal/makeDeposit";
     }
 }
