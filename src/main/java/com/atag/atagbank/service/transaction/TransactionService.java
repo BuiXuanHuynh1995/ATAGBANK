da1f9b98fccb5530afe03b6d99d9cad022ee08df
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-06-23 16:33:59 +0700

    create template

--- src/main/java/com/atag/atagbank/service/transaction/TransactionService.java
+++ src/main/java/com/atag/atagbank/service/transaction/TransactionService.java
@@ -1,15 +1,14 @@
-package com.atag.atagbank.service.transaction.impl;
+package com.atag.atagbank.service.transaction;
 
 import com.atag.atagbank.model.Transaction;
 import com.atag.atagbank.repository.TransactionRepository;
-import com.atag.atagbank.service.transaction.ITransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TransactionServiceImpl implements ITransactionService {
+public class TransactionService implements ITransactionService {
 
     @Autowired
     TransactionRepository transactionRepository;
