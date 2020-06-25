2642154c0eb332191a2044ba282c30daf2912e82
linhhm2406 <linh.hm24@gmail.com>
2020-06-25 22:20:06 +0700

    set username + role display on navbar on homepage

--- src/main/java/com/atag/atagbank/controller/MainController.java
+++ src/main/java/com/atag/atagbank/controller/MainController.java
@@ -8,6 +8,8 @@ import com.atag.atagbank.service.role.IRoleService;
 import com.atag.atagbank.service.user.MyUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -46,8 +48,21 @@ public class MainController {
     private IConfirmationTokenService confirmationTokenService;
 
     @GetMapping("/")
-    public ModelAndView getHomePage() {
+    public ModelAndView getHomePage(HttpSession session) {
+        MyUser currentUser = getUserFromPrincipal();
+        if (currentUser != null) {
+            session.setAttribute("currentUserName", currentUser.getName() + "-" + currentUser.getRole().getRole());
+        }
         return new ModelAndView("index");
+    }
+
+    private MyUser getUserFromPrincipal() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal != "anonymousUser") {
+            String username = ((UserDetails) principal).getUsername();
+            return myUserService.findByUserName(username);
+        }
+        return null;
     }
 
     @GetMapping("/login")
@@ -57,7 +72,7 @@ public class MainController {
         return modelAndView;
     }
 
-   @PostMapping("/login")
+    @PostMapping("/login")
     public ModelAndView login(@ModelAttribute MyUser currentUser, HttpSession session) {
         MyUser loginUser = myUserService.findByUserName(currentUser.getUsername());
         if (loginUser != null && currentUser.getPassword().equals(loginUser.getPassword())) {
@@ -74,8 +89,8 @@ public class MainController {
         return new ModelAndView("login", "notFound", "Wrong username or password!");
     }
 
-    @RequestMapping(value="/registration", method = RequestMethod.GET)
-    public ModelAndView registration(){
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public ModelAndView registration() {
         ModelAndView modelAndView = new ModelAndView();
         MyUser user = new MyUser();
         modelAndView.addObject("user", user);
@@ -131,12 +146,10 @@ public class MainController {
         }
     }
 
-    @RequestMapping(value="/confirm-account", method= {RequestMethod.GET})
-    public ModelAndView confirmUserAccount(ModelAndView modelAndView, @RequestParam("token")String confirmationToken)
-    {
+    @RequestMapping(value = "/confirm-account", method = {RequestMethod.GET})
+    public ModelAndView confirmUserAccount(ModelAndView modelAndView, @RequestParam("token") String confirmationToken) {
         ConfirmationToken token = confirmationTokenService.findByConfirmationToken(confirmationToken);
-        if(token != null)
-        {
+        if (token != null) {
             MyUser user = token.getUser();
             user.setEnabled(true);
             Account account = new Account();
@@ -147,10 +160,8 @@ public class MainController {
             user.setAccount(account);
             myUserService.save(user);
             modelAndView.setViewName("accountVerified");
-        }
-        else
-        {
-            modelAndView.addObject("message","The link is invalid or broken!");
+        } else {
+            modelAndView.addObject("message", "The link is invalid or broken!");
             modelAndView.setViewName("error");
         }
 
@@ -214,11 +225,11 @@ public class MainController {
         return new ModelAndView("error");
     }
 
-    @RequestMapping(value = "/newPassword",method = {RequestMethod.GET, RequestMethod.POST})
+    @RequestMapping(value = "/newPassword", method = {RequestMethod.GET, RequestMethod.POST})
     public ModelAndView editUser(@ModelAttribute MyUser user) {
         ModelAndView modelAndView = new ModelAndView("newPassword");
-        if (user.getPassword().length()<6){
-            modelAndView.addObject("message","Passowrd length must be between 6 and 15");
+        if (user.getPassword().length() < 6) {
+            modelAndView.addObject("message", "Passowrd length must be between 6 and 15");
         }
         if (!myUserService.isCorrectConfirmPassword(user)) {
             modelAndView.addObject("message", "Your confirm password is incorrect");
@@ -234,7 +245,7 @@ public class MainController {
     }
 
     @GetMapping("/accessDenied")
-    public ModelAndView showAccessDeniedPage(){
-        return new ModelAndView("error","message","You don't have access to go to this page");
+    public ModelAndView showAccessDeniedPage() {
+        return new ModelAndView("error", "message", "You don't have access to go to this page");
     }
 }
