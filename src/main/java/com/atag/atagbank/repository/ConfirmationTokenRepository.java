c0309a79e1c4c58eb042aeff3cecec32bad69ae9
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-06-26 11:42:58 +0700

    remove api

--- src/main/java/com/atag/atagbank/repository/ConfirmationTokenRepository.java
+++ src/main/java/com/atag/atagbank/repository/ConfirmationTokenRepository.java
@@ -2,7 +2,9 @@ package com.atag.atagbank.repository;
 
 import com.atag.atagbank.model.ConfirmationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken,String> {
     ConfirmationToken findByConfirmationToken(String confirmationToken);
 }
