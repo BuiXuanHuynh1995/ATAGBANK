4a7fb3e1885e1e049e7588a60c2358e831307be4
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-06-26 11:37:53 +0700

    add annotation

--- src/main/java/com/atag/atagbank/repository/MyUserRepository.java
+++ src/main/java/com/atag/atagbank/repository/MyUserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface MyUserRepository extends PagingAndSortingRepository<MyUser, Long>{
     MyUser findByUsername(String username);
     MyUser findByName(String name);
--- src/main/java/com/atag/atagbank/repository/OtpCodeRepository.java
+++ src/main/java/com/atag/atagbank/repository/OtpCodeRepository.java
@@ -2,10 +2,11 @@ package com.atag.atagbank.repository;
 
 import com.atag.atagbank.model.OtpCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
-
+@Repository
 public interface OtpCodeRepository extends JpaRepository<OtpCode,Long> {
     Optional<OtpCode>  findByEmail(String email);
     @Transactional
